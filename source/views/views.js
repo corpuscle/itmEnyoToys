949dffc925f6b9e1101e4eae8f0c46d5bc4172d9
Ian Miller <git@och.me.uk>
2015-01-11 15:35:23 +0000

    Aim to standardise general app structure

--- source/views/views.js
+++ source/views/views.js
@@ -1,22 +1,18 @@
 enyo.kind({
     name: "equivalents.MainView",
-    kind: "FittableRows",
+    kind: "Panels",
+    arrangerKind: "CollapsingArranger",
     components:[
+	{kind: "enyo.AppMenu", style: "overflow: hidden;", components: [
+	    { kind: "enyo.AppMenuItem", content: "Help", ontap: "doNowt" }
+	]},
 	{kind: "Panels", fit: true, components: [
 	     {name: "degreesAndMinutesView", kind: "equivalents.DecimalsAndMinutes"},
 	     {name: "paceAndSpeedView", kind: "equivalents.PaceAndSpeed"},
 	     {name: "altitude", kind: "equivalents.Altitude"},
 	     {name: "schoolYears", kind: "equivalents.SchoolYears"},
 	     {name: "aboutBox", kind: "equivalents.About"}
-	]},
-	{
-	    kind: "enyo.AppMenu",
-            style: "overflow: hidden;",
-	    components: [
-//		{ kind: "enyo.AppMenuItem", content: "Help", ontap: "" }
-		{ content: "Help", ontap: "doNowt" }
-	    ]
-	}
+	]}
     ],
     doNowt: function () {
     }
