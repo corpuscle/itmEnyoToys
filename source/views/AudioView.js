8215bbca99e9f895dccfc5c427d18850fc718183
Ian Miller <git@och.me.uk>
2016-01-10 22:46:13 +0000

    Offer wav and mp3 separately

--- source/views/AudioView.js
+++ source/views/AudioView.js
@@ -2,10 +2,15 @@ enyo.kind({
     name: "enyoToys.AudioView",
     components:[
 	{kind: "onyx.Toolbar", content: "Audio"},
-	{kind: "Audio", name: "mySound", src: "assets/Flurry.mp3"},
-	{kind: "enyo.Button", content: "Play", name: "button", ontap: "playSound"}
+	{kind: "Audio", name: "myMP3", src: "assets/Flurry.mp3"},
+	{kind: "Audio", name: "myWav", src: "assets/notification.wav"},
+	{kind: "enyo.Button", content: "MP3", name: "mp3Button", ontap: "playMP3"},
+	{kind: "enyo.Button", content: "wav", name: "wavButton", ontap: "playWav"}
     ],
-    playSound: function() {
-	this.$.mySound.play();
+    playMP3: function() {
+	this.$.myMP3.play();
+    },
+    playWav: function() {
+	this.$.myWav.play();
     }
 });
